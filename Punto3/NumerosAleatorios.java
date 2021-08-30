ce7e8b4fc4f089ad75f68f37ba91972c6332928c
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-08-29 21:01:59 -0300

    punto3- 100 aleatorios entre -100 y 100

---
+++ Punto3/NumerosAleatorios.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class NumerosAleatorios {
+    static final int MIN_VALOR = -100;
+    static final int RANGO = 200;
+    static final int CANTNUMEROS = 100;
+    static final String NOMBRE_ARCHIVO = "./numeros.txt";
+
+    public static void main(String[] args) {
+        try{
+            BufferedWriter buff = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO));
+            for (int i = 0; i < CANTNUMEROS ; i++){
+                double num = MIN_VALOR + Math.random() * (RANGO);
+                buff.write( num +"\n");
+            }
+            buff.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.err.println(ex.getMessage() + "\nSignifica que el archivo del "
+                    + "que queriamos leer no existe.");
+        }
+        catch (IOException ex) {
+            System.err.println("Error leyendo o escribiendo en algun archivo.");
+        }
+    }
+}
